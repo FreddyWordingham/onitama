78e957cc928cc088e11814e33e992bad0891e98d
FreddyWordingham <freddy@digilab.co.uk>
2023-02-18 17:29:13 +0000

    Added login button

--- api/main.py
+++ api/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
 
 from . import settings
+
+users = {"freddy": {"password": "1234"}, "lizbeth": {"password": "1234"}}
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -11,3 +14,17 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.get("/", response_class=HTMLResponse)
 async def homepage(request: Request):
     return settings.TEMPLATES.TemplateResponse("homepage.html", {"request": request})
+
+
+class inputLogin(BaseModel):
+    username: str
+    password: str
+
+
+@app.post("/login")
+async def login(input: inputLogin):
+    if input.username not in users:
+        return False
+    if input.password != users[input.username]["password"]:
+        return False
+    return True
